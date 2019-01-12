e716c55e102d4d6bcc7ba401bf2864fdd31b580e
Jorge Guijarro <guijarro101@hotmail.com>
2019-01-12 17:00:14 +0000

    code js

--- Publico/PrimerProyecto/code.js
+++ Publico/PrimerProyecto/code.js
@@ -5,6 +5,9 @@ var size;
 var price;
 var maker;
 var urlpic;
+var clickItems = document.getElementById("aqui");
+var req;
+var item;
 
 function getDataTshirts(){
     req = new XMLHttpRequest();
@@ -16,26 +19,46 @@ function getDataTshirts(){
         }
         return dataTshirts;
     }
+}
 
-var clickToItems = document.getElementById("aqui");
-clickToItems.addEventListener("click", drawDataTshirt)
 
 function catchDataTshirtById(idTshirt){
     dataTshirts.tshirts.forEach(item){
     if (idTshirt === item.id){
         model = item.model;
-        colour = item.colour
-        size = item.size
-        price = item.price
-        maker = item.maker
-        urlpic = item.urlpic
+        colour = item.colour;
+        size = item.size;
+        price = item.price;
+        maker = item.maker;
+        urlpic = item.urlpic;
         }
     }
 }
 
 function drawDataTshirt(idTshirt){
-    //1: capturar informacion de la camiseta
-    catchDataTshirtById(idTshirt)
-    //2: generar dinamicamente el html con la info de la camiseta capturada
+  //0: llamada Ajax
+  getDataTshirts();
+  //1: capturar informacion de la camiseta
+  catchDataTshirtById(idTshirt);
+  //2: generar dinamicamente el html con la info de la camiseta capturada
+    document.getElementById("model").innerHTML=model;
+    document.getElementById("colour").innerHTML=colour;
+    document.getElementById("size").innerHTML=size;
+    document.getElementById("price").innerHTML=price;
+    document.getElementById("maker").innerHTML=maker;
+    document.getElementById("urlpic").innerHTML="<img src=\"{urlpic}\" height=\"100\" width=\"50\">";
 }
 
+function delay(milliseconds){
+  let starttime = new Date();
+  let endtime;
+    do {
+      endtime = new Date();
+    } while (endtime - starttime <= milliseconds);
+    }
+
+// main program
+
+clickItems.addEventListener("click", drawDataTshirt());
+delay(3000);
+location.href= "camiseta.html";
